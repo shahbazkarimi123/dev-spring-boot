91d326ba95afcd34b98401a5a311295ea17eda0f
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2023-06-19 02:57:39 +0530

    postconstruct and preDestroy

--- 02-spring-boot-core/08-bean-lifecycle-methods/src/main/java/com/karimi/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-core/08-bean-lifecycle-methods/src/main/java/com/karimi/springcoredemo/common/CricketCoach.java
@@ -1,16 +1,29 @@
 package com.karimi.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component //@component annotation marks as spring bean 
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE )
+
 public class CricketCoach implements Coach {
 
     public CricketCoach(){
         System.out.println("In constructor: "+ getClass().getSimpleName());
     }
+
+    //define init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): "+getClass().getSimpleName() );
+    }
+    //define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
 
     @Override
     public String getDailyworkout() {
