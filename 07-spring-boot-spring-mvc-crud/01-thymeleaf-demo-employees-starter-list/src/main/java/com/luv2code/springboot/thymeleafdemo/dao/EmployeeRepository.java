7556ed3a65e47aab6d190cedb41dfafc92f86002
shahbazkarimi123 <shahbazkarimi123@gmail.com>
2023-07-19 01:20:06 +0530

    spring boot mvc security with tymeleaf

--- 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -7,5 +9,8 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+
+	// add a method to short by last name	to understand this watch l-170
+	public List<Employee> findAllByOrderByLastNameAsc();
 	
 }
